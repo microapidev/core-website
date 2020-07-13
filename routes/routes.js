56424fc0253de0d2871c2be2e906c66b2905b01d
tonymj76 <demeyortj@gmail.com>
2020-07-13 01:34:31 +0100

    routes

---
+++ routes/routes.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const user = require('../controllers/usercontroller');
+const {
+  home,
+  documentation,
+  signup,
+  signIn,
+  blog,
+  blogpost,
+} = require('../controllers/home');
+
+const appRoute = express.Router();
+
+appRoute.get('/', home);
+appRoute.get('/documentation', documentation);
+appRoute.get('/sign-up', signup);
+appRoute.get('/sign-in', signIn);
+appRoute.get('/blog', blog);
+appRoute.get('/blog-post', blogpost);
+
+appRoute.get('/dashboard', user.isAuthenticated, (req, res) => {
+  res.render('index', { variable: 'Hello Guys' });
+});
+
+appRoute.get('/usage', user.isAuthenticated, (req, res) => {
+  res.render('usage');
+});
+
+appRoute.get('/settings', user.isAuthenticated, (req, res) => {
+  res.render('settings');
+});
+
+appRoute.get('/billings', user.isAuthenticated, (req, res) => {
+  res.render('billings');
+});
+
+appRoute.get('/authapi', user.isAuthenticated, (req, res) => {
+  res.render('authapi');
+});
+
+appRoute.get('/authset', user.isAuthenticated, (req, res) => {
+  res.render('authset');
+});
+
+// appRoute.get('/login', user.isAuthenticated, (req, res) => {
+//   const { successMsg } = req.query;
+//   res.render('Login', {
+//     successMsg,
+//   });
+// });
+//
+// appRoute.get('/forgot-password', user.isAuthenticated, (req, res) => {
+//   const { successMsg } = req.query;
+//   res.render('Pages/Forgotpassword', {
+//     successMsg,
+//   });
+// });
+//
+// appRoute.get('/changepassword', user.isAuthenticated, (req, res) => {
+//   const { token } = req.query;
+//   if (!token) return res.redirect('/login');
+//   res.cookie('changepasstoken', token);
+//   return res.render('Pages/Changepassword');
+// });
+
+appRoute.post('/login', user.isAuthenticated, user.login);
+appRoute.post('/register', user.isAuthenticated, user.signup);
+appRoute.post('/forgot-password', user.forget);
+appRoute.get('/logout', user.logout);
+appRoute.get('/dashboard', user.dashboard);
+appRoute.get('/googleauth', user.isAuthenticated, user.googleauth);
+appRoute.get('/google/callback', user.isAuthenticated, user.googlecallback);
+appRoute.post('/changepassword', user.isAuthenticated, user.changepassword);
+
+module.exports = appRoute;
